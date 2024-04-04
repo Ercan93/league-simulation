ed8059137f29a1778c4ec8f2555f10bc13606bb1
Ercan Uzunsakal <ercan.uzunsakal@protonmail.com>
2024-04-05 01:16:11 +0300

    Update Club interface in club.ts

--- src/domain/club/club.ts
+++ src/domain/club/club.ts
@@ -3,15 +3,17 @@ import { MatchRecord } from './matchRecord';
 import { ClubStats } from './clubStats';
 
 export interface Club {
+  id: number;
   clubName: string;
   clubShortName: string;
   leagueName: string;
+  abbr: string;
   clubStats: ClubStats;
   matchRecords: {
     homeRecord: MatchRecord;
     awayRecord: MatchRecord;
   };
-  matchFixtures: {
+  matchFixtures?: {
     pastMatches: MatchFixture[];
     futureMatches: MatchFixture[];
     mostRecentMatch: MatchFixture;
