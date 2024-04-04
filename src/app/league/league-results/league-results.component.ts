4c59b067273fc3c7822f7b34b9b5f98f46430b3f
Ercan Uzunsakal <ercan.uzunsakal@protonmail.com>
2024-04-04 22:31:34 +0300

    Add league-results component to league module

---
+++ src/app/league/league-results/league-results.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-league-results',
+  templateUrl: './league-results.component.html',
+  styleUrl: './league-results.component.scss'
+})
+export class LeagueResultsComponent {
+
+}
--- src/app/league/league.module.ts
+++ src/app/league/league.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LeagueFixturesComponent } from './league-fixtures/league-fixtures.component';
 import { LeagueTableComponent } from './league-table/league-table.component';
+import { LeagueResultsComponent } from './league-results/league-results.component';
 
 
 
 @NgModule({
   declarations: [
     LeagueFixturesComponent,
-    LeagueTableComponent
+    LeagueTableComponent,
+    LeagueResultsComponent
   ],
   imports: [
     CommonModule
