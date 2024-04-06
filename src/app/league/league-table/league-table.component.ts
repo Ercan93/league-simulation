0a2ba89b9c1e442f99202aeb85a554e9d2b49f6e
Ercan Uzunsakal <ercan.uzunsakal@protonmail.com>
2024-04-06 20:51:19 +0300

    Update league-table component to display club statistics in a table

--- src/app/league/league-table/league-table.component.ts
+++ src/app/league/league-table/league-table.component.ts
@@ -1,10 +1,20 @@
+import { Club } from './../../../domain/club/club';
+import { LeagueService } from './../../../service/leagueservice';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-league-table',
   templateUrl: './league-table.component.html',
-  styleUrl: './league-table.component.scss'
+  styleUrl: './league-table.component.scss',
 })
 export class LeagueTableComponent {
+  clubs: Club[] = [];
 
+  constructor(private LeagueService: LeagueService) {}
+
+  ngOnInit(): void {
+    this.LeagueService.season$.subscribe((season) => {
+      this.clubs = season.clubs;
+    });
+  }
 }
