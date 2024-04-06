4c79decacaac57184cdc42d86343bbef9bf2e02d
Ercan Uzunsakal <ercan.uzunsakal@protonmail.com>
2024-04-06 21:54:36 +0300

    Add LeagueActionsComponent for league actions UI

--- src/app/league/league.module.ts
+++ src/app/league/league.module.ts
@@ -4,20 +4,30 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LeagueFixturesComponent } from './league-fixtures/league-fixtures.component';
 import { LeagueTableComponent } from './league-table/league-table.component';
 import { LeagueResultsComponent } from './league-results/league-results.component';
+import { LeagueActionsComponent } from './league-actions/league-actions.component';
 import { TableModule } from 'primeng/table';
 import { FieldsetModule } from 'primeng/fieldset';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [
     LeagueFixturesComponent,
     LeagueTableComponent,
     LeagueResultsComponent,
+    LeagueActionsComponent,
   ],
-  imports: [CommonModule, TableModule, FieldsetModule, BrowserAnimationsModule],
+  imports: [
+    CommonModule,
+    TableModule,
+    FieldsetModule,
+    ButtonModule,
+    BrowserAnimationsModule,
+  ],
   exports: [
     LeagueFixturesComponent,
     LeagueTableComponent,
     LeagueResultsComponent,
+    LeagueActionsComponent,
   ],
 })
 export class LeagueModule {}
