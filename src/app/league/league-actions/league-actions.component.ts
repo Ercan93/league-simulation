5e8a6d565ce2e2fbf6f00c3f06b53a5554ee0869
Ercan Uzunsakal <ercan.uzunsakal@protonmail.com>
2024-04-06 21:53:21 +0300

    Add LeagueActionsComponent for league actions UI

---
+++ src/app/league/league-actions/league-actions.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-league-actions',
+  templateUrl: './league-actions.component.html',
+  styleUrl: './league-actions.component.scss'
+})
+export class LeagueActionsComponent {
+
+}
