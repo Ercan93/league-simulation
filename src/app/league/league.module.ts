04dc108bf99dfd39d3d45b32c8370f343f379728
Ercan Uzunsakal <ercan.uzunsakal@protonmail.com>
2024-04-06 20:51:42 +0300

    Update LeagueModule imports and declarations

--- src/app/league/league.module.ts
+++ src/app/league/league.module.ts
@@ -1,24 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LeagueFixturesComponent } from './league-fixtures/league-fixtures.component';
 import { LeagueTableComponent } from './league-table/league-table.component';
 import { LeagueResultsComponent } from './league-results/league-results.component';
-
-
+import { TableModule } from 'primeng/table';
+import { FieldsetModule } from 'primeng/fieldset';
 
 @NgModule({
   declarations: [
     LeagueFixturesComponent,
     LeagueTableComponent,
-    LeagueResultsComponent
+    LeagueResultsComponent,
   ],
-  imports: [
-    CommonModule
-  ],
+  imports: [CommonModule, TableModule, FieldsetModule, BrowserAnimationsModule],
   exports: [
     LeagueFixturesComponent,
     LeagueTableComponent,
-    LeagueResultsComponent
-  ]
+    LeagueResultsComponent,
+  ],
 })
-export class LeagueModule { }
+export class LeagueModule {}
