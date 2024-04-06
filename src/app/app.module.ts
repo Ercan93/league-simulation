dd2be4de720caad3e4160a9df29451ffa50662fe
Ercan Uzunsakal <ercan.uzunsakal@protonmail.com>
2024-04-06 21:52:59 +0300

    import TabViewModule and TabMenuModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LeagueModule } from './league/league.module';
 import { HomeComponent } from './components/home/home.component';
+import { TabViewModule } from 'primeng/tabview';
+import { TabMenuModule } from 'primeng/tabmenu';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule, LeagueModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    LeagueModule,
+    TabViewModule,
+    TabMenuModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
