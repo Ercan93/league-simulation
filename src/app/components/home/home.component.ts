c445f1f0a7a59d8ba31db3fc7e7178655c9e209b
Ercan Uzunsakal <ercan.uzunsakal@protonmail.com>
2024-04-06 20:53:01 +0300

    Update home component to include league table and fixtures

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { LeagueService } from '../../../service/leagueservice';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-
+  constructor(private leagueService: LeagueService) {
+    this.leagueService.createSeason(
+      'Super Lig',
+      '2023-2024',
+      'Eylül 2023',
+      'Mayıs 2024'
+    );
+    console.log('Season created');
+  }
 }
