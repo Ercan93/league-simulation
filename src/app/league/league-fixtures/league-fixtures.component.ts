d3d3153bd703325ba62d8b67e5ad0b7a0745421b
Ercan Uzunsakal <ercan.uzunsakal@protonmail.com>
2024-04-06 20:51:28 +0300

    Update league fixtures component to display match fixtures in a structured layout

--- src/app/league/league-fixtures/league-fixtures.component.ts
+++ src/app/league/league-fixtures/league-fixtures.component.ts
@@ -1,10 +1,36 @@
+import { Observable, range } from 'rxjs';
+import { map, toArray, switchMap } from 'rxjs/operators';
 import { Component } from '@angular/core';
+import { MatchFixture } from '../../../domain/club/matchFixture';
+import { LeagueService } from '../../../service/leagueservice';
+import { Season } from '../../../domain/season';
 
 @Component({
   selector: 'app-league-fixtures',
   templateUrl: './league-fixtures.component.html',
-  styleUrl: './league-fixtures.component.scss'
+  styleUrl: './league-fixtures.component.scss',
 })
 export class LeagueFixturesComponent {
+  fixtures: MatchFixture[][] = [];
 
+  constructor(private leagueService: LeagueService) {}
+
+  ngOnInit(): void {
+    this.getMatchweeks(this.leagueService.season$).subscribe(
+      (matchweeks) => (this.fixtures = matchweeks)
+    );
+  }
+
+  getMatchweeks(season$: Observable<Season>): Observable<MatchFixture[][]> {
+    return season$.pipe(
+      switchMap((season) =>
+        range(1, (season.clubs.length - 1) * 2).pipe(
+          map((i) =>
+            season.matchFixtures.filter((fixture) => fixture.matchweek === i)
+          ),
+          toArray()
+        )
+      )
+    );
+  }
 }
