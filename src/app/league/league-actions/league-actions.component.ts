ce871f62eb0f2d6eaaa8fc4607d7fc3f7a1b7589
Ercan Uzunsakal <ercan.uzunsakal@protonmail.com>
2024-04-07 05:09:55 +0300

    Update LeagueActionsComponent to include simulation functions

--- src/app/league/league-actions/league-actions.component.ts
+++ src/app/league/league-actions/league-actions.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { LeagueService } from '../../../service/leagueservice';
 
 @Component({
   selector: 'app-league-actions',
   templateUrl: './league-actions.component.html',
-  styleUrl: './league-actions.component.scss'
+  styleUrl: './league-actions.component.scss',
 })
 export class LeagueActionsComponent {
+  constructor(private leagueService: LeagueService) {}
 
+  simulationWeek(): void {
+    this.leagueService.simulateWeek();
+  }
+
+  simulationSeason(): void {
+    this.leagueService.simulateSeason();
+  }
 }
